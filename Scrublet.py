4b3f468e7db5ecb494f242f547cb5e4ea24424d0
nimanouri-nn <nima.nouri@sanofi.com>
2023-01-11 13:57:38 +0000

    scrublet seed

--- Scrublet.py
+++ Scrublet.py
@@ -8,7 +8,7 @@ from scipy.sparse import csc_matrix
 
 def scrublet_py(i, j, val, dim):
 	data = csc_matrix((val, (i, j)), shape = dim)
-	scrub = scr.Scrublet(data)
+	scrub = scr.Scrublet(data, random_state=123)
 	doublet_scores, predicted_doublets = scrub.scrub_doublets()
 	return(doublet_scores, scrub.doublet_scores_sim_) # predicted_doublets, 
 
